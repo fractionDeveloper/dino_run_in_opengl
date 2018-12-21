ea38127422490ae1a8d90c37246cc84b32f9f039
u34rali <u34r@hotmail.com>
2018-12-21 21:32:59 +0500

    update  display using timer function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void drawGround();
 void myTimer(int);
@@ -11,8 +13,11 @@ void dontDoAnythingIdle();
 #define PI 3.1415
 #define theta 90.0f
 
-bool startGame = false;
 
+bool startGame = false;
+bool gameRunning = false;
+
+int timerTime = 1;
 float mX = 0;
 float dinoVerticalPosition = 0;
 float dinoPoints[2][32] = {{6, 6, 7, 7, 5, 5, 3, 3, 4, 4, 2, 2, 0, 2, 3, 5, 7, 7, 13, 13, 9, 9, 12, 12, 8, 8, 10, 10, 9, 9, 8, 8},
@@ -27,10 +32,61 @@ float treePoints[2][16] = {{125, 125, 122, 122, 123, 123, 125, 125, 128, 128, 13
                            {0, 8, 8, 12, 12, 9, 9, 13, 13, 8, 8, 11, 11, 7, 7, 0}};
 
 float dinoRunningLegs = false;
+
+void drawString(float x, float y, float z, char *string)
+{
+    glRasterPos3f(x, y, z);
+
+    for (char *c = string; *c != '\0'; c++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *c); // Updates the position
+    }
+}
+
+// void beginText()
+// {
+//    glMatrixMode(GL_PROJECTION);
+
+//    // Save the current projection matrix
+//    glPushMatrix();
+
+//    // Make the current matrix the identity matrix
+//    glLoadIdentity();
+
+//    // Set the projection (to 2D orthographic)
+//    gluOrtho2D(0, width, 0, height);
+
+//    glMatrixMode(GL_MODELVIEW);
+// }
+
+/**
+ * End the state for text 
+ */
+// void endText()
+// {
+//    glMatrixMode(GL_PROJECTION);
+
+//    // Restore the original projection matrix
+//    glPopMatrix();
+
+//    glMatrixMode(GL_MODELVIEW);
+// }
+
 void Draw_Figure()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.0, 0.0, 0.0);
+
+    // beginText();
+
+    // Render the text
+    if (!startGame)
+    {
+        drawString(30.0, 50.0, 0.0, "Press Up Arrow to start game");
+    }
+
+    // Set things up for normal rendering
+    // endText();
 
     // float dinoEye[2] = {13,13,12,12}
     glPointSize(5.0);
@@ -58,7 +114,7 @@ void Draw_Figure()
 
     if (startGame && dinoVerticalPosition <= 0 && dinoRunningLegs)
     {
-        
+
         glBegin(GL_POLYGON);
         glVertex2f(5, 1);
         glVertex2f(7, 1);
@@ -115,10 +171,7 @@ void drawGround()
 
 void drawTree()
 {
-    if(treeLastPointPosition<=13 && treeLastPointPosition>=0  && dinoVerticalPosition < 13){
-        printf("tree inside dragon range value is = %d \n" , treeLastPointPosition);
-        glutIdleFunc(dontDoAnythingIdle);
-    }
+
     if (treeLastPointPosition < 0)
     {
         for (int i = 0; i < 16; i++)
@@ -144,10 +197,36 @@ void drawTree()
     }
     treeLastPointPosition--;
     glEnd();
+
+    if (treeLastPointPosition <= 13 && treeLastPointPosition >= 0 && dinoVerticalPosition < 13)
+    {
+        // printf("tree inside dragon range value is = %d \n", treeLastPointPosition);
+        // glutPostRedisplay();
+        drawString(30.0, 50.0, 0.0, "Game Over press UP arrow to play again");
+        glutIdleFunc(dontDoAnythingIdle);
+        glutTimerFunc(0 , myTimerDoNothing , 0);
+        gameRunning = false;
+        startGame = false;
+        // return;
+    }
 }
 
 void dinoPixels()
 {
+}
+
+void gameOverFunc()
+{
+    // glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 0.0);
+    glPushMatrix();
+    glLoadIdentity();
+    drawString(30.0, 50.0, 0.0, "Game Over press UP arrow to play again");
+    // glTranslatef(0.5, 0.5, 0.0);
+    // glRotatef(25.0, 0.0, 0.0, 1.0);
+    // glutSolidCube(0.5);
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 void display()
@@ -159,7 +238,7 @@ void display()
     glutSwapBuffers();
 }
 
-int V = 25;
+int V = 22;
 int gravity = -10;
 
 float VX = V * cos(theta * PI / 180);
@@ -185,27 +264,27 @@ void jumpFunc()
 {
     if (jump)
     {
+        dinoVerticalPosition = VY * time + 0.5 * (gravity)*time * time;
         if (dinoVerticalPosition >= 0)
         {
-            time = time + 0.05;
+            time = time + 0.1;
             // mX = VX * time;
-            dinoVerticalPosition = VY * time + 0.5 * (gravity)*time * time;
 
-            // printf("%f--\n", dinoVerticalPosition);
+            // printf("%f--\n\n\n", dinoVerticalPosition);
             for (int i = 0; i < 32; i++)
             {
                 tempDinoPoints[1][i] = dinoPoints[1][i] + dinoVerticalPosition;
 
-                // printf("%f----\n",dinoPoints[1][i]);
+                // printf("%f----\n", dinoPoints[1][i]);
             }
             // tempDinoEye[0] = dinoEye[0]+dinoVerticalPosition;
             tempDinoEye[1] = dinoEye[1] + dinoVerticalPosition;
-            // glutTimerFunc(50, myTimerDoNothing, 0);
+            // glutTimerFunc(timerTime, myTimerDoNothing, 0);
         }
         else
         {
             // glutIdleFunc(dontDoAnythingIdle); // 1 click == 1 jump
-            // glutTimerFunc(50, myTimer, 0);
+            // glutTimerFunc(timerTime, myTimer, 0);
             time = 0.1;
             dinoVerticalPosition = 0;
             jump = false;
@@ -221,7 +300,7 @@ void myTimer(int value)
         jumpFunc();
         glutPostRedisplay();
     }
-    glutTimerFunc(50, myTimer, 0);
+    glutTimerFunc(timerTime, myTimer, 0);
 }
 void myTimerDoNothing(int value)
 {
@@ -235,11 +314,12 @@ void keyDown(int key, int x, int y)
         {
             jump = true;
             // glutTimerFunc(50, myTimer, 0);
-            glutIdleFunc(myIdleFunc);
+            // glutIdleFunc(myIdleFunc);
         }
         else
         {
             startGame = true;
+            gameRunning = true;
         }
     }
 }
@@ -264,12 +344,12 @@ int main(int argc, char **argv)
     // int wid =GLUT_SCREEN_HEIGHT;
     // printf("%d",wid);
 
-    glutCreateWindow("");
+    glutCreateWindow("Dino Run");
 
     glutDisplayFunc(display);
 
     // glutIdleFunc(myIdleFunc);
-    // glutTimerFunc(50, myTimer, 0); // mili seconds , myTimer(int value), value
+    glutTimerFunc(timerTime, myTimer, 0); // mili seconds , myTimer(int value), value
     // glutKeyboardFunc(myKeyboardFunc); // myKeyboardFunc(unsigned char key , int x , int y)
     // glutMouseFunc(myMouse); //  myMouse(int button , int state , int x , int y)
     glutSpecialFunc(keyDown); // keyDown(int key , int x , int y)
